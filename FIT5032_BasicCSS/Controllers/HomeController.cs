680fd885c08cefb3f2a7702d74b43b341b502acf
Julius Jones <jjon0014@student.monash.edu>
2023-08-06 20:30:24 +1000

    Add project files.

---
+++ FIT5032_BasicCSS/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using FIT5032_BasicCSS.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace FIT5032_BasicCSS.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ FIT5032_BasicCSS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FIT5032_BasicCSS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
